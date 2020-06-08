57a264a6f1771f69c712200102006209bd0128b7
Stephanie Raymos <stephanieraymos@gmail.com>
2020-06-07 18:44:28 -0700

    Added dispatch in Favorites

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,12 +4,18 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
         tips: state.tips,
         favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {
+    deleteFavorite: campsiteId => (deleteFavorite(campsiteId))
 };
 
 class Favorites extends Component {
@@ -20,13 +26,13 @@ class Favorites extends Component {
 
     render() {
         const { navigate } = this.props.navigation;
-        const renderFavoriteItem = ({item}) => {
+        const renderFavoriteItem = ({ item }) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                    onPress={() => navigate('AdminInfo', {tipsId: item.id})}
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                    onPress={() => navigate('AdminInfo', { tipsId: item.id })}
                 />
             );
         };
@@ -53,4 +59,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
